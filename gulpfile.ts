ded4994159386ba9e246d8d4108726921eb7e7bc
Xavier Zheng <Xavier_CHN@Live.com>
2022-06-20 16:26:11 +0800

    Update gulpfile.ts

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,6 +1,6 @@
 import gulp from 'gulp';
-import path from 'path';
 import * as dotax from './src';
+import path from 'path';
 
 const paths: { [key: string]: string } = {
     excels: 'excels',
@@ -9,20 +9,25 @@ const paths: { [key: string]: string } = {
     game_resource: 'game/resource',
 };
 
+/**
+ * @description 将excel文件转换为kv文件
+ * @description Convert your excel file to kv file
+ */
 const cSheetToKV = async () => {
     gulp.src(`${paths.excels}/*.{xlsx,xls}`)
         .pipe(
             dotax.sheetToKV({
                 sheetsIgnore: '^__', // 忽略以两个下划线开头的sheet
                 indent: `	`, // 自定义缩进
-                customPinyins: {
-                    远看山有色近听水无声: 'yuan kan shan you se jin ting shui wu sheng',
-                },
             })
         )
         .pipe(gulp.dest(paths.kv));
 };
 
+/**
+ * @description 将kv文件转换为panorama使用的js文件，你需要在webpack.config.js中配置相应的loader
+ * @description Convert your kv file to panorama js file, you need to configure the loader in webpack.config.js
+ */
 const cKVToJS = async () => {
     gulp.src(`${paths.kv}/**/*.{kv,txt}`)
         .pipe(
@@ -33,13 +38,19 @@ const cKVToJS = async () => {
         .pipe(gulp.dest(`${paths.panorama}/src/utils`));
 };
 
+/**
+ * @description 从 kv 文件中提取所有的description，你可以使用 customPrefix 和 customSuffix 之类的参数来指定自己的前缀和后缀
+ * @description Extract all description from kv file, you can use customPrefix and customSuffix to specify your prefix and suffix
+ */
 const cKVToLocal = async () => {
     gulp.src(`${paths.kv}/**/*.{kv,txt}`).pipe(
         dotax.kvToLocalsCSV(`${paths.game_resource}/addon.csv`, {
             customPrefix: (key, data, path) => {
                 if (data.BaseClass && /ability_/.test(data.BaseClass)) {
-                    if (/combos/.test(path)) {
+                    if (data.ScriptFile && data.ScriptFile.startsWith('abilities/combos/')) {
                         return 'dota_tooltip_ability_combo_';
+                    } else if (data.ScriptFile && /^/.test(data.ScriptFile)) {
+                        return 'dota_tooltip_ability_chess_ability_';
                     } else {
                         return 'dota_tooltip_ability_';
                     }
@@ -48,8 +59,7 @@ const cKVToLocal = async () => {
             },
             customSuffix: (key, data, path) => {
                 let suffix = [''];
-                // 这个范例是如果说某个技能是羁绊的KV文件出来的，那么给他做一个每个等级的后缀
-                if (/combos/.test(path)) {
+                if (data.ScriptFile && data.ScriptFile.startsWith('abilities/combos/')) {
                     suffix = ['_description'];
                     let maxLevel = data.MaxLevel;
                     if (maxLevel) {
@@ -65,14 +75,27 @@ const cKVToLocal = async () => {
     );
 };
 
+/**
+ * @description 将 addon.csv 中的本地化文本转换为 addon_*.txt 文件，这个 task 建议 kv 写完了之后再运行一次即可
+ * @description Convert addon.csv local text to addon_*.txt file， this task is recommended to run after kv is finished
+ *
+ */
 const cCSVToLocalization = async () => {
     gulp.src(`${paths.game_resource}/addon.csv`).pipe(dotax.csvToLocals(paths.game_resource));
 };
 
+/**
+ * @description 将现有的 addon_*.txt 文件转换为 addon.csv 文件，这个 task 是为了使这个task适配你原有的开发方式，如果是重新开发，则无需运行这个task
+ * @description Convert addon_*.txt file to addon.csv file, this task is for adapting your original development method, if you are re-developing, you don't need to run this task
+ */
 const cLocalsToCSV = async () => {
     dotax.localsToCSV(`${paths.game_resource}/addon_*.txt`, `${paths.game_resource}/addon.csv`);
 };
 
+/**
+ * 将panorama/images目录下的jpg,png,psd文件集合到 dest 目录中的 image_precache.css文件中
+ * 使用这个 task ，你可以在 game setup 阶段的时候，将所有的图片都编译而不用自己写
+ */
 gulp.task(`img_pcache`, async () => {
     await gulp
         .src(`content/panorama/images/**/*.{jpg,png,psd}`)
@@ -82,6 +105,9 @@ gulp.task(`img_pcache`, async () => {
 gulp.task('img_pcache:watch', async () => {
     gulp.watch(`content/panorama/images/**/*.{jpg,png,psd}`, gulp.series('img_pcache'));
 });
+
+// 以下的task，顾名思义即可
+gulp.task('local', cLocalsToCSV);
 
 gulp.task('sheetToKV', cSheetToKV);
 gulp.task('sheetToKV:watch', () => {
@@ -105,10 +131,11 @@ gulp.task('csvToLocalization:watch', (callback: Function) => {
     callback();
 });
 
-gulp.task('makeCSV', cLocalsToCSV); // 这个任务一般只需要执行一次，用来把现有的locals文件转换为csv文件
 gulp.task('predev', gulp.series('sheetToKV', 'kvToJS', 'csvToLocalization', 'img_pcache'));
 gulp.task('dev', gulp.parallel('sheetToKV:watch', 'csvToLocalization:watch', 'img_pcache:watch'));
 gulp.task('build', gulp.series('predev', 'csvToLocalization'));
 gulp.task('jssync', gulp.series('sheetToKV', 'kvToJS'));
 gulp.task('kvl', gulp.series('kvToLocal'));
+
+// 编译发布版本
 gulp.task('prod', gulp.series('sheetToKV', 'kvToJS', 'csvToLocalization', 'img_pcache'));

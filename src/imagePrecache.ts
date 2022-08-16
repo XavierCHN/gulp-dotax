d6bc6160628c1d1a7766b18c900706eca184e794
Xavier Zheng <Xavier_CHN@Live.com>
2022-08-16 09:33:41 +0800

    split image precache into multiple files to prevent compile issue

--- src/imagePrecache.ts
+++ src/imagePrecache.ts
@@ -2,7 +2,7 @@ import Vinyl from 'vinyl';
 import path from 'path';
 import through2 from 'through2';
 
-export function imagePrecacche(root: string, fileName?: string) {
+export function imagePrecacche(root: string) {
     let files: string[] = [];
     let firstFile: Vinyl;
     function collect(file: Vinyl, enc: any, next: Function) {
@@ -20,17 +20,23 @@ export function imagePrecacche(root: string, fileName?: string) {
     function write(done: Function) {
         if (firstFile == null) return done();
         files = files.sort();
-        const content = `.image-precache {\n${files
-            .map((filename) => `\tbackground-image: url("file://{images}/${filename}");`)
-            .join('\n')}\n}`;
-        const file = new Vinyl({
-            cwd: firstFile.cwd,
-            base: firstFile.base,
-            path: path.join(firstFile.base, `${fileName || 'image_precache'}.css`),
-            contents: Buffer.from(content),
-        });
-        //@ts-ignore
-        this.push(file);
+
+        let fileGroups: string[][] = [];
+        for (let i = 0; i < files.length; i += 500) {
+            fileGroups.push(files.slice(i, i + 500));
+        }
+            
+        fileGroups.forEach((files, i) => {
+            const content = `.image-precache {\n${files.map((filename) => `\tbackground-image: url("file://{images}/${filename}");`).join("\n")}\n}`;
+            const file = new Vinyl({
+                cwd: firstFile.cwd,
+                base: firstFile.base,
+                path: path.join(firstFile.base, `image_precache${i}.css`),
+                contents: Buffer.from(content),
+            });
+            //@ts-ignore
+            this.push(file);
+        })
     }
     return through2.obj(collect, write);
 }

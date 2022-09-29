44129b3910b32ea8be69e5b3fdbcd3d0af0ddb16
Xavier Zheng <Xavier_CHN@live.com>
2022-09-29 21:56:44 +0800

    auto convert cell includes `#` or `|` into arrays

--- src/kvToJS.ts
+++ src/kvToJS.ts
@@ -8,7 +8,15 @@ const keyvalues = require('keyvalues-node');
 
 const PLUGIN_NAME = `gulp-dotax:kvToJS`;
 
-export function kvToJS() {
+export interface KVToJSOptions {
+    // 是否将含有 ArraySeperator 的字符串转换为 array
+    AutoConvertToArray?: boolean;
+    /** 数组的分隔符 目前是 竖杠 | 和 # 号 */
+    ArraySeperator?: string;
+}
+
+export function kvToJS(options?: KVToJSOptions) {
+    const { AutoConvertToArray = true, ArraySeperator = /[\|#]/ } = options ?? {};
     function parseKV(file: Vinyl, enc: any, next: Function) {
         if (file.isNull()) {
             return next(null, file);
@@ -23,13 +31,22 @@ export function kvToJS() {
             const kvKeys = Object.keys(kv);
             const kvData = kvKeys.length === 1 ? kv[kvKeys[0]] : kv;
 
-            // convert all numberic string to number in kvData json
-            // deep run over all objects
             const jsonData = JSON.stringify(
                 kvData,
                 (key, value) => {
+                    // 如果是数字，直接输出数字
                     if (typeof value === 'string' && !isNaN(Number(value))) {
                         return Number(value);
+                    }
+                    // 如果是用 '|' 或者 '#' 分割的字符串，那么直接输出成数组
+                    // 如果AutoConvertToArray为true的话
+                    // 默认为true
+                    if (
+                        AutoConvertToArray &&
+                        typeof value === 'string' &&
+                        ArraySeperator.test(value)
+                    ) {
+                        return value.split(ArraySeperator).map((v) => v.trim());
                     }
                     return value;
                 },

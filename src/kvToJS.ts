9a01a3af74c77bac51b552769767f070c4a118bb
Xavier(Home) <Xavier_CHN@live.com>
2024-12-04 18:24:34 +0800

    update easy-keyvalues from 1.0.12 to 1.2.7 to support #base merging

--- src/kvToJS.ts
+++ src/kvToJS.ts
@@ -3,7 +3,7 @@ import PluginError from 'plugin-error';
 import through from 'through2';
 import Vinyl from 'vinyl';
 import path from 'path';
-import { KeyValues, LoadKeyValuesSync, AutoLoadKeyValuesBaseSync } from 'easy-keyvalues';
+import { KeyValues } from 'easy-keyvalues';
 const cli = require('cli-color');
 
 const PLUGIN_NAME = `gulp-dotax:kvToJS`;
@@ -13,8 +13,6 @@ export interface KVToJSOptions {
     AutoConvertToArray?: boolean;
     /** 数组的分隔符 目前是 竖杠 | 和 # 号 */
     ArraySeperator?: string;
-    /** 是否自动合并#base */
-    AutoMergeBases?: boolean;
 }
 
 export function kvToJS(options?: KVToJSOptions) {
@@ -34,24 +32,11 @@ export function kvToJS(options?: KVToJSOptions) {
 
         try {
             const kvFileName = file.path;
-            const kv = LoadKeyValuesSync(kvFileName);
+            const kv = await KeyValues.Load(kvFileName);
             let kvData = kv.toObject();
             delete kvData['#base']; // 移除所有的 #base，他们不需要被输出
             kvData = kvData[Object.keys(kvData)[0]] ?? {}; // 有可能主文件里面除了 #base 以外没有任何内容
-            // 将所有的 #base 的内容合并到root
-            // 这里有可能会有循环调用，因此不做递归
-            // 坏处是不支持多层级的 #base
-            // todo: 支持多层级的 #base
-            if (AutoMergeBases) {
-                AutoLoadKeyValuesBaseSync(kv, path.dirname(kvFileName));
-                kv.GetChildren().forEach((child) => {
-                    if (child.Key === '#base') {
-                        const childObject = child.toObject();
-                        delete childObject['#base'];
-                        Object.assign(kvData, childObject[Object.keys(childObject)[0]]);
-                    }
-                });
-            }
+            
             let jsonData = JSON.stringify(
                 kvData,
                 (key, value) => {
